1de3d10b52b749e0732e6ad397257950308d97f1
Jose Angel Colin Najera <josecolin99@gmail.com>
2024-06-26 20:04:41 -0600

    update(learn.js): Add learn

---
+++ 02-operadores/07-orden.js
@@ -0,0 +1,10 @@
+let operation = 8/2*(2+2)
+console.log(operation)
+
+
+/*
+    1. Parentesis
+    2. Exponentes (**. raiz cuadrada)
+    3. Multiplicacion y Divicion (*, / de izq --> a der)
+    4. Adicion y Substraccion (+, - de izq --> a der)
+*/
